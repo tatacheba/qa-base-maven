ed9bbcbd500f1dfc61ce8e959b14c3eef27e4cb9
Tata.Chebotar <57943361+tatacheba@users.noreply.github.com>
2024-09-30 13:40:04 +0200

    New learn autotesting

--- src/main/java/org/dmitryyarygin/Calculator.java
+++ src/main/java/org/dmitryyarygin/Calculator.java
@@ -8,4 +8,13 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+    public double division(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return (double) a / b;
+    }
 }
--- src/test/java/CalculatorTests.java
+++ src/test/java/CalculatorTests.java
@@ -2,17 +2,36 @@ import org.dmitryyarygin.Calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 class CalculatorTests {
     @Test
     void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(4, calculator.add(2, 3), "2 + 3 should equal 5");
+        assertEquals(5, calculator.add(2, 3), "2 + 3 should equal 5");
     }
 
     @Test
     void testSubtract() {
         Calculator calculator = new Calculator();
         assertEquals(1, calculator.subtract(3, 2), "3 - 2 should equal 1");
+    }
+    @Test
+    void testMultiplication() {
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.multiplication(3, 2), "3 * 2 should equal 6");
+    }
+    @Test
+    void testDivision() {
+        Calculator calculator = new Calculator();
+        assertEquals(1.5, calculator .division(3, 2), "3 / 2 should equal 1.5");
+    }
+    @Test
+    void divisionByZeroTest() {
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.division(3, 0);
+        }, "Division by zero should throw ArithmeticException");
     }
 }
